8436d52ce1ac7a5dfe47e8656636c63a05cc7709
Paul <krepa098@gmail.com>
2024-04-05 22:17:52 +0200

    updated script

--- scale_up_gif.sh
+++ scale_up_gif.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 ffmpeg -stream_loop 0 -i $1 -crf 32 -vf scale=640:-1 -sws_flags neighbor $2
-ffmpeg -i $2 $1.gif
+ffmpeg -i $2 -filter_complex "fps=10,scale=320:-1:flags=lanczos,split[s0][s1];[s0]palettegen=max_colors=32[p];[s1][p]paletteuse=dither=bayer" $1.gif
